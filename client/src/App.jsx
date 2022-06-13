b4781a60cac70001572045e2330a4a87203861ee
Ryan <jukkasc2@gmail.com>
2022-06-12 20:30:20 -0400

    Setup redux store

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,16 +4,23 @@ import Home from "./pages/Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./style.css";
 
+// Redux
+
+import { Provider } from "react-redux";
+import store from "./store";
+
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Router>
+      </div>
+    </Provider>
   );
 }
 
